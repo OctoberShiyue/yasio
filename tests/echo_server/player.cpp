223d88ec1083823217f1e977b621b59785c4f985
十月 <1023261581@qq.com>
2024-11-12 21:37:51 +0800

    tb

--- tests/echo_server/player.cpp
+++ tests/echo_server/player.cpp
@@ -1,37 +1,4 @@
-#include "yasio/yasio.hpp"
-#include <signal.h>
-
-using namespace yasio;
-
-static yasio::highp_time_t getTimeStamp() { return yasio::highp_clock<yasio::system_clock_t>() / 1000; }
-
-class Player {
-public:
-  Player(io_service* service, io_base* th);
-  ~Player();
-
-  void Login(int64_t uid);
-
-  void UpdateHeartbeat() { this->online_time = getTimeStamp(); };
-
-  int64_t GetUid() const { return this->uid; };
-
-  highp_time_t online_time;
-  highp_time_t login_time;
-
-  cxx17::string_view aes_key = "";
-
-private:
-  unsigned int id;
-  int64_t uid;
-
-  io_base* ib;
-  io_service* service;
-
-  deadline_timer_ptr onetime;
-
-  deadline_timer_ptr heartbeat_time;
-};
+#include <player.h>
 
 Player::Player(io_service* service, io_base* th)
 {
