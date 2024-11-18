b11621c73c6ec5c80c8888b453628bdeccf05400
十月 <1023261581@qq.com>
2024-11-18 10:20:29 +0800

    1

--- tests/echo_server/cmd.h
+++ tests/echo_server/cmd.h
@@ -1,7 +1,7 @@
 #ifndef _CMD_H_
 #define _CMD_H_
 #include <connectionPool.h>
-
+#include <conio.h>
 std::map<std::string, std::function<void(std::vector<std::string> args)>> gCmds;
 
 void HelpCmd(std::vector<std::string> args)
@@ -16,36 +16,75 @@ void LoadLuaCodeCmd(std::vector<std::string> args)
   std::ostringstream oss;
   for (const auto& str : args)
   {
-    oss << str + " "; 
+    oss << str + " ";
   }
   std::string code = oss.str();
 
   ConnectionPool::loadLuaCode(code);
 }
 
+std::string getInputFromDialog()
+{
+  std::ofstream script("input.vbs");
+  script << "Dim input\n";
+  script << "input = InputBox(\"Please enter your input:\" & vbCrLf & \"-h: help\" & vbCrLf & \"-lua [scripts code] run lua scripts\", \"Input Box\")\n";
+  script << "If input <> \"\" Then\n";
+  script << "   WScript.Echo input\n";
+  script << "End If\n";
+  script.close();
+
+  std::string result;
+  char buffer[128];
+  FILE* pipe = _popen("cscript //nologo input.vbs", "r");
+
+  if (pipe)
+  {
+    while (fgets(buffer, sizeof(buffer), pipe) != NULL)
+    {
+      result += buffer;
+    }
+    _pclose(pipe);
+  }
+
+  std::remove("input.vbs");
+
+  return result;
+}
+
 void InitCmd()
 {
   gCmds["-h"]   = HelpCmd;
   gCmds["-lua"] = LoadLuaCodeCmd;
-  std::string input;
+
   while (true)
   {
-    std::getline(std::cin, input);
-
-    std::istringstream iss(input);
-    std::string command;
-    std::vector<std::string> args;
-    iss >> command;
-
-    std::string arg;
-    while (iss >> arg)
+    if (_kbhit())
     {
-      args.push_back(arg);
+      int ch = _getch();
+      if (ch == 0 || ch == 224)
+      {
+        ch = _getch();
+        if (ch == 63)
+        {
+          std::string input = getInputFromDialog();
+          std::istringstream iss(input);
+          std::string command;
+          std::vector<std::string> args;
+          iss >> command;
+
+          std::string arg;
+          while (iss >> arg)
+          {
+            args.push_back(arg);
+          }
+          if (gCmds[command])
+          {
+            gCmds[command](args);
+          }
+        }
+      }
     }
-    if (gCmds[command])
-    {
-      gCmds[command](args);
-    }
+    Sleep(100);
   }
 }
 
